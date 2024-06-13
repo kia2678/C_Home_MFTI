5b1484bd41182594a8c85961f2e1721c92e499a2
kia2678 <kia261978@gmail.com>
2024-06-13 22:47:50 +0300

    DZ1

---
+++ DZ1/a1.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("	 Let's\n");
+    printf("           go\n");
+    printf("               to walk");
+
+    return 0;
+}
