8ef629a6b260bc09e79ce2b77c89bfbd361fa703
kia2678 <kia261978@gmail.com>
2024-06-20 23:13:53 +0300

    файлы по дз3

---
+++ DZ3/a12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned short a;
+    scanf("%d", &a);
+    unsigned short b = a/100;
+    unsigned short c = (a%100)/10;
+    unsigned short d = a%10;
+    printf("%d\n", b+c+d);
+    return 0;
+}
